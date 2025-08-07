e13f518fffae21afb265270b99a7f480bba5be0c
gaman <gaman@MSI>
2025-08-07 18:29:07 +0530

    WindowHandlesConcept

---
+++ src/main/java/SeleniumPractise25/SeleniumPractise25/NewWindowConcept.java
@@ -0,0 +1,21 @@
+package SeleniumPractise25.SeleniumPractise25;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class NewWindowConcept {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.orangehrm.com/en/contact-sales");
+		
+		driver.switchTo().newWindow(WindowType.WINDOW);//4.x
+		
+		driver.get("https://google.com");
+		//driver.close();
+		
+		
+		
+
+	}
+}
