8452617989e4978045983d45874bbcbf1b71f419
gaman <gaman@MSI>
2025-08-13 22:49:03 +0530

    WindowSizeHandling

---
+++ src/main/java/SeleniumPractise25/SeleniumPractise25/WindowSize.java
@@ -0,0 +1,23 @@
+package SeleniumPractise25.SeleniumPractise25;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowSize {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+
+		
+		Dimension dim = new Dimension(768, 1024);
+
+		driver.manage().window().setSize(dim);
+
+		driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/register");
+
+	}
+
+}
+
